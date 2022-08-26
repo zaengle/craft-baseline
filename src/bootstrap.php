2e78cea14a456fb49d3ee855df0d90f28f3ec4ab
Tom Davies <tom@tomdavies.net>
2022-08-26 15:43:21 +0100

    refactor to move .env / composer.json to project root

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,14 +5,15 @@
 
 // Define path constants
 define('CRAFT_BASE_PATH', __DIR__);
-define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH . '/vendor');
+define('DOTENV_PATH', dirname(CRAFT_BASE_PATH, 1));
+define('CRAFT_COMPOSER_PATH', dirname(CRAFT_BASE_PATH, 1) . '/composer.json');
+define('CRAFT_VENDOR_PATH', dirname(CRAFT_BASE_PATH, 1) . '/vendor');
 
 // Load Composer's autoloader
 require_once CRAFT_VENDOR_PATH . '/autoload.php';
 
-// Load dotenv?
-if (class_exists(Dotenv\Dotenv::class)) {
+if (class_exists(Dotenv\Dotenv::class) && file_exists(DOTENV_PATH . '/.env')) {
     // By default, this will allow .env file values to override environment variables
     // with matching names. Use `createUnsafeImmutable` to disable this.
-    Dotenv\Dotenv::createUnsafeMutable(CRAFT_BASE_PATH)->safeLoad();
+    Dotenv\Dotenv::createUnsafeMutable(DOTENV_PATH)->safeLoad();
 }
