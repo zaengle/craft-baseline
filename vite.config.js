b59c08a7e6745529ee8ad37fd3d805a2c441fb47
Rachel Opperman <oppermanre17@gmail.com>
2023-02-10 15:55:07 -0500

    Removes unused Vite plugin

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import copy from 'rollup-plugin-copy'
 import tailwindcss from 'tailwindcss'
 import ViteRestart from 'vite-plugin-restart'
 import ViteSvgSpriteWrapper from 'vite-svg-sprite-wrapper'
-import ViteRequireContext from '@originjs/vite-plugin-require-context'
 
 export default defineConfig(({ command }) => ({
   base: command === 'serve' ? '' : '/dist/',
@@ -50,7 +49,6 @@ export default defineConfig(({ command }) => ({
       restart: ['./tailwind.config.js'],
       reload: ['src/templates/**/*', 'src/modules/**/*.php'],
     }),
-    ViteRequireContext(),
     ViteSvgSpriteWrapper({
       icons: 'src/assets/svg/sprite/*.svg',
       outputDir: 'src/web/dist/svg/sprite',
