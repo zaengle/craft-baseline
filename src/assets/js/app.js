a44f1b8ffc4280e28bf41d0e336cd8ac3b6e9efb
Rachel Opperman <34202440+movntains@users.noreply.github.com>
2023-02-14 16:00:36 -0500

    Feature/16 update fe build to use vite (#19)
    * Replaces Mix with Vite
    
    * Fixes janky hot reloading
    
    * Removes twigpack package
    
    * Gets SVG sprites working
    
    * Adds Vite env var to example env
    
    * Removes unused Vite plugin
    
    * Specifies directory for Composer install
    
    * Updates working directory path
    
    * Fixes more failed jobs
    
    * Fixes typo
    
    * Tries to run phpstan from root
    
    * Tries to get phpstan job working
    
    * Switches to separate action for PHPStan
    
    * Updates path to vendor files in PHPStan config
    
    * Increases PHPStan memory limit
    
    * Switches to package for PHPStan config
    
    * Updates PHPStan action path
    
    * Changes to configuration key
    
    * Fixes path in PHPStan config
    
    * Tries making paths less specific
    
    * Tries using a script instead
    
    * Adds working directory
    
    * Tries pointing to config file
    
    * Comments out PHPStan job to test ECS job
    
    * Updates ECS check script
    
    * Changes ECS script back
    
    * Tries scanning vendor directories
    
    * Deletes unneeded stubs
    
    * Updates Editorconfig
    
    * Removes unneeded phpstan script
    
    * Tries running ECS directly
    
    * Removes with property
    
    * Removes unneeded ECS scripts
    
    * Adds ECS & PHPStan scripts back

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,11 @@
-import './lib/loadSvgSprite'
-
 function init() {
   console.log('hello world!')
 }
 
 window.addEventListener('DOMContentLoaded', init)
+
+if (import.meta.hot) {
+  import.meta.hot.accept(() => {
+    console.log('HMR')
+  })
+}
---
+++ vite.config.js
@@ -0,0 +1,67 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import copy from 'rollup-plugin-copy'
+import tailwindcss from 'tailwindcss'
+import ViteRestart from 'vite-plugin-restart'
+import ViteSvgSpriteWrapper from 'vite-svg-sprite-wrapper'
+
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '' : '/dist/',
+  build: {
+    manifest: true,
+    emptyOutDir: true,
+    outDir: 'src/web/dist',
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    rollupOptions: {
+      input: {
+        app: 'src/assets/js/app.js',
+        css: 'src/assets/css/app.css',
+      },
+      output: {
+        sourcemap: true,
+      },
+    },
+  },
+  css: {
+    postcss: {
+      to: 'dist',
+      from: 'src/assets/css/app.css',
+      plugins: [
+        require('postcss-import'),
+        require('tailwindcss/nesting'),
+        tailwindcss('./tailwind.config.js'),
+        require('postcss-focus-visible'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+  plugins: [
+    copy({
+      hook: 'writeBundle',
+      targets: [
+        { src: './src/assets/img/**/*', dest: './src/web/dist/img' },
+        { src: './src/assets/svg/**/*', dest: './src/web/dist/svg' },
+      ],
+    }),
+    ViteRestart({
+      restart: ['./tailwind.config.js'],
+      reload: ['src/templates/**/*', 'src/modules/**/*.php'],
+    }),
+    ViteSvgSpriteWrapper({
+      icons: 'src/assets/svg/sprite/*.svg',
+      outputDir: 'src/web/dist/svg/sprite',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve('./src/assets/js'),
+    },
+  },
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    strictPort: true,
+  },
+}))
