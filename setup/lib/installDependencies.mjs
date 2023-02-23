6734f178eedcbfa4463e9403edf725c5381f0b62
Tom Davies <tom@tomdavies.net>
2023-02-23 14:08:17 +0000

    fixed: actually install npm deps

--- setup/lib/installDependencies.mjs
+++ setup/lib/installDependencies.mjs
@@ -5,6 +5,7 @@ export default async ({ answers }) => {
     if (answers.installDeps) {
       console.log('Installing dependencies...')
       await command('ddev composer install')
+      await command('ddev npm install')
     }
     return { answers }
   } catch (error) {
