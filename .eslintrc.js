a6c716dbc481783fab13524b26b94d489d45deb4
Tom Davies <tom@tomdavies.net>
2024-02-01 15:22:10 +0000

    fixed: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
-  env: {
-    'vitest-globals/env': true,
-  },
-  extends: '@zaengle/eslint-config-base',
+  extends: []
+    '@zaengle/eslint-config-base',
+    'plugin:vitest-globals/recommended'
+  ],
+   "overrides": [
+    {
+      "files": ["**/__tests__/*.{j,t}s?(x)", "**/*.spec.{j,t}s?(x)"],
+      "env": {
+        "vitest-globals/env": true,
+      },
+    },
+  ],
 }
