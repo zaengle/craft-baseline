0a7eff928118fef404863cf6d176af130c09e6eb
Tom Davies <tom@tomdavies.net>
2024-03-21 14:04:06 +0000

    build: convert general config to use builder API

--- src/config/general.php
+++ src/config/general.php
@@ -9,8 +9,8 @@
  *
  * **NOTE**
  *
- * In general it's often better to set values that will vary
- * between environments via via an environment variable
+ * In general, it's often better to set values that will vary
+ * between environments via an environment variable
  * override, rather than adding explicit config here.
  *
  * That will also permit changing settings in development
@@ -25,70 +25,26 @@
  * - CRAFT_CACHE_DURATION
  */
 
+use craft\config\GeneralConfig;
 use craft\helpers\App;
 
-$isDev = App::env('CRAFT_ENVIRONMENT') === 'dev';
 $isProd = App::env('CRAFT_ENVIRONMENT') === 'production';
 
-return [
-
-    // Whether generated URLs should omit "index.php"
-    'omitScriptNameInUrls' => true,
-
-    // Whether crawlers should be allowed to index pages and following links
-    'disallowRobots' => !$isProd,
-
-
-
-    'errorTemplatePrefix' => '_errors/',
-
-    /**
-     * Assets
-     */
-    'brokenImagePath' => '@webroot/static/404.svg',
-    'convertFilenamesToAscii' => true,
-    'revAssetUrls' => true,
-
-    /**
-     * Caching
-     */
-    'enableTemplateCaching' => App::parseBooleanEnv('CRAFT_ENABLE_TEMPLATE_CACHING') ?? true,
-
-    /**
-     * Dates / times
-     */
-    // Default Week Start Day (0 = Sunday, 1 = Monday...)
-    'defaultWeekStartDay' => 0,
-    'timezone' => 'America/New_York',
-
-    /**
-     * Environment
-     */
-    'aliases' => [
+return GeneralConfig::create()
+    ->disallowRobots(!$isProd)
+    ->errorTemplatePrefix('_errors/')
+    ->brokenImagePath('@webroot/static/404.svg')
+    ->enableTemplateCaching(App::parseBooleanEnv('CRAFT_ENABLE_TEMPLATE_CACHING') ?? true)
+    ->defaultWeekStartDay(0)
+    ->timezone('America/New_York')
+    ->aliases([
         '@web' => App::env('PRIMARY_SITE_URL'),
         '@webroot' => dirname(__DIR__) . '/web',
-    ],
-
-    /**
-     * Search
-     */
-    // Enable fuzzy search by default
-    'defaultSearchTermOptions' => [
+    ])
+    ->defaultSearchTermOptions([
         'subLeft' => true,
         'subRight' => true,
-    ],
-
-    /**
-     * Security
-     */
-    'cpTrigger' => 'manage',
-    'enableGql' => false,
-    'preventUserEnumeration' => true,
-    'sendPoweredByHeader' => false,
-
-    /**
-     * Users
-     */
-    'autoLoginAfterAccountActivation' => true,
-    'useEmailAsUsername' => true,
-];
+    ])
+    ->cpTrigger('manage')
+    ->enableGql(false)
+    ->sendPoweredByHeader(false);
