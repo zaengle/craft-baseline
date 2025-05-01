f1b9ed197ceb843cf94c82d1bd8b0043847d52f4
Jenny <jenny@zaengle.com>
2025-05-01 09:46:07 -0400

    Content model templates (#158)
    Co-authored-by: Jenny Medeiros <jennymedeiros@Mac.medeiroshome>

--- setup/lib/setupCraft.mjs
+++ setup/lib/setupCraft.mjs
@@ -3,12 +3,12 @@ import { command } from './utils.mjs'
 export default async ({ answers }) => {
   try {
     if (answers.installCraft) {
-      console.log('Installing Craft...')
-      await command("ddev craft install/craft --site-name=\$PRIMARY_SITE_NAME --site-url=\$PRIMARY_SITE_URL")
-      await command('ddev craft plugin/install ckeditor')
-      await command('ddev craft plugin/install conventions')
-      await command('ddev craft plugin/install toolbelt')
-      await command('ddev craft plugin/install vite')
+      console.log('Setting up Craft...')
+      await command('ddev import-db --file=./setup/db/seed.sql')
+      await command('ddev craft up')
+      console.log('-------------------------------------')
+      console.log('Set up admin user:')
+      await command('ddev craft users/create --admin')
       await command('ddev mutagen sync')
     }
     return { answers }
