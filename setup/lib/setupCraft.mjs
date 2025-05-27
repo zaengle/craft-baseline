e37215fb06713679a2da15e2a37385f4ccfcf3d1
Tom Davies <tom@tomdavies.net>
2025-05-27 10:28:35 +0100

    build: apply craft changes on setup non-interactively

--- setup/lib/setupCraft.mjs
+++ setup/lib/setupCraft.mjs
@@ -5,7 +5,7 @@ export default async ({ answers }) => {
     if (answers.installCraft) {
       console.log('Setting up Craft...')
       await command('ddev import-db --file=./setup/db/seed.sql')
-      await command('ddev craft up')
+      await command('ddev craft up --interactive=0 --no-backup')
       console.log('-------------------------------------')
       console.log('Set up admin user:')
       await command('ddev craft users/create --admin')
